77c1d07a4426db0e601fe14d257e6103187bc0d7
Filippos Dourachalis <filippos.dourachalis@gmail.com>
2024-01-17 16:19:00 +0200

    Add custom datastore and static ARP

--- code/plc/plc.py
+++ code/plc/plc.py
@@ -30,6 +30,7 @@ usage::
 """
 import asyncio
 import logging
+import csv
 
 from pymodbus import __version__ as pymodbus_version
 from pymodbus.datastore import (
@@ -53,8 +54,21 @@ logging.basicConfig()
 _logger = logging.getLogger(__file__)
 _logger.setLevel(logging.INFO)
 
+def read_datablocks(file_path):
+    datablocks = {}
+    try:
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                register_type = row['register_type']
+                address = int(row['start_address'])
+                values = [ int(x) for x in row['values'].split(',') ]
+                datablocks[register_type] = ModbusSequentialDataBlock(address, values)
+    except FileNotFoundError:
+        print("Error: Config file not found")
+    return datablocks
 
-def setup_server(description=None, context=None, cmdline=None):
+def setup_server(description=None, context=None, cmdline=None, config_filepath=None):
     """Run server setup."""
     args = get_commandline(server=True, description=description, cmdline=cmdline)
     if context:
@@ -77,45 +91,14 @@ def setup_server(description=None, context=None, cmdline=None):
             # full address range::
             datablock = ModbusSequentialDataBlock.create()
 
-        if args.slaves:
-            # The server then makes use of a server context that allows the server
-            # to respond with different slave contexts for different slave ids.
-            # By default it will return the same context for every slave id supplied
-            # (broadcast mode).
-            # However, this can be overloaded by setting the single flag to False and
-            # then supplying a dictionary of slave id to context mapping::
-            #
-            # The slave context can also be initialized in zero_mode which means
-            # that a request to address(0-7) will map to the address (0-7).
-            # The default is False which is based on section 4.4 of the
-            # specification, so address(0-7) will map to (1-8)::
-            context = {
-                0x01: ModbusSlaveContext(
-                    di=datablock,
-                    co=datablock,
-                    hr=datablock,
-                    ir=datablock,
-                ),
-                0x02: ModbusSlaveContext(
-                    di=datablock,
-                    co=datablock,
-                    hr=datablock,
-                    ir=datablock,
-                ),
-                0x03: ModbusSlaveContext(
-                    di=datablock,
-                    co=datablock,
-                    hr=datablock,
-                    ir=datablock,
-                    zero_mode=True,
-                ),
-            }
-            single = False
-        else:
-            context = ModbusSlaveContext(
-                di=datablock, co=datablock, hr=datablock, ir=datablock
-            )
-            single = True
+        datablocks = read_datablocks(config_filepath)
+        context = ModbusSlaveContext(
+            di=datablocks.get("di", ModbusSequentialDataBlock(0x00, [17] * 100)),
+            co=datablocks.get("co", ModbusSequentialDataBlock(0x00, [17] * 100)), 
+            hr=datablocks.get("hr", ModbusSequentialDataBlock(0x00, [17] * 100)), 
+            ir=datablocks.get("ir", ModbusSequentialDataBlock(0x00, [17] * 100))
+        )
+        single = True
 
         # Build data storage
         args.context = ModbusServerContext(slaves=context, single=single)
@@ -147,8 +130,6 @@ async def run_async_server(args):
         server = await StartAsyncTcpServer(
             context=args.context,  # Data storage
             identity=args.identity,  # server identify
-            # TBD host=
-            # TBD port=
             address=address,  # listen address
             # custom_functions=[],  # allow custom handling
             framer=args.framer,  # The framer strategy to use
@@ -220,7 +201,7 @@ async def run_async_server(args):
 async def async_helper():
     """Combine setup and run."""
     _logger.info("Starting...")
-    run_args = setup_server(description="Run asynchronous server.")
+    run_args = setup_server(description="Run asynchronous server.", config_filepath="./config/config.csv")
     await run_async_server(run_args)
 
 
